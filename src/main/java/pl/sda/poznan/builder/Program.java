56094c6d55ab2282f87fb8399e8c34a70f765550
Michal Madajewski <mw.madajewski@gmail.com>
2018-03-03 11:34:11 +0100

    Add builder pattern parser

--- src/main/java/pl/sda/poznan/builder/Program.java
+++ src/main/java/pl/sda/poznan/builder/Program.java
@@ -1,34 +1,10 @@
 package pl.sda.poznan.builder;
 
-import java.time.LocalDate;
-
 public class Program {
+
     public static void main(String[] args) {
-//        Reservation reservation = new Reservation();
-//        reservation.setCity("Poznań");
-//        reservation.setHeadcount(199);
-
-        Reservation.ReservationBuilder rb = Reservation.builder()
-                .city("Poznań")
-                .headCount(100)
-                .price(99d);
-
-        // odczyt z pilku
-        rb.date(LocalDate.parse("2018-03-03"));
-
-        // zakoncz proces budowania - zwro gotowy obiekt
-
-        Reservation build = rb.build();
-        System.out.println(build);
-
-        // inny scenariusz - znam wszystkie wartosci od razu
-
-        Reservation wroclaw = Reservation.builder()
-                .date(LocalDate.parse("2018-03-03"))
-                .headCount(100)
-                .price(20)
-                .city("Wrocław")
-                .build();
-
+        String toParse = "Date, 2018-02-12, Headcount, 250, City, Poznan, Price, 100";
+        Reservation myReservation = ReservationParser.parse(toParse);
+        System.out.println(myReservation);
     }
 }
---
+++ src/main/java/pl/sda/poznan/builder/ReservationParser.java
@@ -0,0 +1,33 @@
+package pl.sda.poznan.builder;
+
+import java.time.LocalDate;
+
+import pl.sda.poznan.builder.Reservation.ReservationBuilder;
+
+public class ReservationParser {
+
+    // Sample format:
+    // Date, 2018-02-12, Headcount, 250, City, Poznan, Price, 100
+
+    public static Reservation parse(String s) {
+
+        String[] split = s.split(", ");
+        ReservationBuilder rb = Reservation.builder();
+
+        for (int i = 0; i < split.length; i += 2) {
+            String key = split[i];
+            String value = split[i + 1];
+            if (key.toLowerCase().equals("date")) {
+                rb.date(LocalDate.parse(value));
+            } else if (key.toLowerCase().equals("headcoubt")) {
+                rb.headCount(Integer.parseInt(value));
+            } else if (key.toLowerCase().equals("city")) {
+                rb.city(value);
+            } else if (key.toLowerCase().equals("price")) {
+                rb.price(Double.parseDouble(value));
+            }
+        }
+        return rb.build();
+
+    }
+}
