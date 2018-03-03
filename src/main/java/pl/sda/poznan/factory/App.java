1b6353f0fdbc6eaa77c7f4735d1d2cb69e7fecf7
Michal Madajewski <mw.madajewski@gmail.com>
2018-03-03 15:07:00 +0100

    Add factory design patern

--- src/main/java/pl/sda/poznan/factory/App.java
+++ src/main/java/pl/sda/poznan/factory/App.java
@@ -6,6 +6,8 @@ import pl.sda.poznan.factory.writer.CompanyWriter;
 import pl.sda.poznan.factory.writer.TxtCompanyWriter;
 import pl.sda.poznan.factory.writer.XmlCompanyWriter;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
@@ -16,9 +18,20 @@ public class App {
         company.addEmployee(dyrektor);
         company.addEmployee(kierownik);
 
-        CompanyWriter writer = new XmlCompanyWriter("firma.xml");
-        writer.write(company);
+//        CompanyWriter writer = new XmlCompanyWriter("firma.xml");
+//        writer.write(company);
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj nazwe pliku, do ktorego chcesz zapisac:");
+
+        String filename = scanner.next();
+        CompanyWriterFactory companyWriterFactory = new CompanyWriterFactory();
+        try {
+            CompanyWriter writer = companyWriterFactory.create(filename);
+            writer.write(company);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
 
     }
 }
---
+++ src/main/java/pl/sda/poznan/factory/Factory.java
@@ -0,0 +1,7 @@
+package pl.sda.poznan.factory;
+
+public interface Factory<K, V> {
+
+    V create(K key);
+
+}
